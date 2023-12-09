175270b3b44ef58e58fcb823f4b1118ab6ab5bea
Ran Har-Shuv <ran.harshuv@gmail.com>
2023-12-09 22:14:43 +0200

    drop animation + improve board view

--- Graphics_Board.py
+++ Graphics_Board.py
@@ -35,13 +35,32 @@ class Graphics_Board:
         i2, j2 = self.view_to_model( self.x_drag_end, self.y_drag_end)
         return round(i1,0) ,round(j1,0) ,round(i2,0) ,round(j2,0)
     def draw_board(self):
-        self.window.setBackground("Black")
+        self.window.setBackground("white")
+        line1= Line(Point (int(6.85*radius), int(2.3*radius)), Point( int(17.15*radius), int(2.3*radius)))
+        line2= Line(Point (int(6.85*radius), int(20.14*radius)), Point( int(17.15*radius), int(20.14 *radius)))
+        line3= Line(Point (int(22.3*radius), int(11.22*radius)), Point( int(17.15*radius), int(20.14 *radius)))
+        line4= Line(Point (int(22.3*radius), int(11.22*radius)), Point( int(17.15*radius), int(2.3*radius)))
+        line5= Line(Point (int(1.7*radius), int(11.22*radius)), Point( int(6.85*radius), int(2.3*radius)))
+        line6= Line(Point (int(1.7*radius), int(11.22*radius)), Point( int(6.85*radius), int(20.14*radius)))
+        p1=Point (int(6.85*radius), int(2.3*radius))
+        p2=Point( int(17.15*radius), int(2.3*radius))
+        p3=Point (int(22.3*radius), int(11.22*radius))
+        p4=  Point( int(17.15*radius), int(20.14 *radius))
+        p5=Point (int(6.85*radius), int(20.14*radius))
+        p6=Point (int(1.7*radius), int(11.22*radius))
+
+        aPolygon = Polygon(p1, p2, p3, p4, p5, p6)
+        aPolygon.draw(self.window)
+        aPolygon.setFill("gray")
+
+
+
         for i in range(5):
             for j in range(5 + i):
-                self.draw_ball(5 - i + j, i + 1, "Gray", radius)
-                self.draw_ball(j + 1, 9 - i, "Gray", radius)
-                self.draw_ball(5 - i + j, i + 1, "Black", radius*0.7)
-                self.draw_ball(j + 1, 9 - i, "Black", radius*0.7)
+                self.draw_ball(5 - i + j, i + 1, "light gray", radius)
+                self.draw_ball(j + 1, 9 - i, "light gray", radius)
+                self.draw_ball(5 - i + j, i + 1, "gray", radius*0.7)
+                self.draw_ball(j + 1, 9 - i, "gray", radius*0.7)
 
     def draw_ball(self, i, j, color, r = radius):
         c = Circle(Point(radius *(2.2 * i +1.1 *j-4.5), radius * (1.7+line*j)), r)
@@ -53,6 +72,19 @@ class Graphics_Board:
             for ball in balls:
                 ball.move(delta_x, delta_y)
             time.sleep(0.05)
+    def fall_ball(self, ball):
+        ball.undraw()
+        center = ball.getCenter()
+        fill = ball.config['fill']
+        for i in range(15):
+            c= Circle (center, int(radius/15*(15-i)) )
+            c.setFill(fill)
+            c.draw(self.window)
+            time.sleep(0.05)
+            c.undraw()
+
+
+
 
     def move_balls(self, balls, dir):
         if dir==Direction.RIGHT:
--- main.py
+++ main.py
@@ -6,11 +6,14 @@ visual_board = Graphics_Board()
 
 #for i in range (4):
     #for j in range (5+i):
-        #visual_board.draw_ball(5-i+j, i+1, "Red")
-        #visual_board.draw_ball(j+1, 9-i, "Blue")
-ball=visual_board.draw_ball(5, 5, "Pink")
-ball2=visual_board.draw_ball(4,5,"pink")
-ball3=visual_board.draw_ball(3,5,"pink")
+        #visual_board.draw_ball(5-i+j, i+1, "black")
+        #visual_board.draw_ball(j+1, 9-i, "white")
+ball=visual_board.draw_ball(5, 5, "white")
+ball2=visual_board.draw_ball(4,5,"white")
+ball3=visual_board.draw_ball(3,5,"white")
+ball4=visual_board.draw_ball(2,5, "black")
+ball5=visual_board.draw_ball(1,5, "black")
+
 mat = [ball,ball2,ball3]
 time.sleep(1)
 
@@ -25,8 +28,11 @@ time.sleep(1)
 visual_board.move_balls(mat, Direction.UP_LEFT)
 time.sleep(1)
 visual_board.move_balls(mat, Direction.UP_RIGHT)
+time.sleep(1)
+visual_board.move_balls([ball,ball2,ball3,ball4,ball5], Direction.LEFT)
+visual_board.fall_ball(ball5)
+x1, y1, x2, y2= visual_board.wait_for_user_move()
+print(x1, y1, x2, y2)
+# x1, y1, x2, y2= visual_board.wait_for_user_move()
+# print(x1, y1, x2, y2)
 
-x1, y1, x2, y2= visual_board.wait_for_user_move()
-print(x1, y1, x2, y2)
-x1, y1, x2, y2= visual_board.wait_for_user_move()
-print(x1, y1, x2, y2)
